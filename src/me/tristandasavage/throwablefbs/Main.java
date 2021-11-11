36faa11d140819c5d53b3d4ea5389807afbe3609
mrgamer45 <72839119+mrgamer45@users.noreply.github.com>
2021-11-10 19:28:59 -0500

    removed something
    removed stuff cuz i forgot

--- src/me/tristandasavage/throwablefbs/Main.java
+++ src/me/tristandasavage/throwablefbs/Main.java
@@ -1,11 +1,5 @@
 package me.tristandasavage.throwablefbs;
 
-import org.bukkit.NamespacedKey;
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-import org.bukkit.enchantments.Enchantment;
-import org.bukkit.entity.Player;
-import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.lang.reflect.Field;
@@ -18,7 +12,6 @@ public class Main extends JavaPlugin {
         this.saveDefaultConfig();
         this.getServer().getPluginManager().registerEvents(new ShootFB(), this);
         this.getLogger().log(Level.INFO, "Throwable Fireballs v1.0 loaded!");
-        this.getServer().getPluginCommand("uwu").setExecutor(new UWU());
     }
 
     @Override
